35f182e817a39be79f57328a05cb00807305cbb8
Ichigo002 <wiktorbojanowski28@wp.pl>
2022-04-06 18:35:32 +0200

    add drag fucntion

--- scripts/start.js
+++ scripts/start.js
@@ -4,7 +4,8 @@ var wins = [];
 
 function Start() {
     wins.push(new Window("Default win", 100, 100));
-    wins[0].setContent("I love Wiki");
     wins[0].createGUI();
     wins[0].setPosition(100,260);
+    wins[0].setContent("I love Wiki");
+    wins[0].SetDraggingEvent();
 }
--- scripts/window.js
+++ scripts/window.js
@@ -1,9 +1,8 @@
 class Window {
-    active = true;
+    
     def_height = 100;
     def_width = 100;
     id_win = 0;
-    content = "NONE";
 
     constructor(name, width, height) {
         this.name = name;
@@ -12,6 +11,7 @@ class Window {
     }
 
     createGUI() {
+        
         let gui = '<div class="win" id="win-' + this.id_win + '">' +
             '<div class="win-top">' + this.name + 
             '<span>' +
@@ -19,14 +19,37 @@ class Window {
             '<i class="icon-window-maximize icon-all" onclick="wins[' + this.id_win + '].action_maxmalise()"></i>' +
             '<i class="icon-minus-1 icon-all" onclick="wins[' + this.id_win + '].action_minimalise()"></i>' +
             '<div style="clear: both;"></div>' +
-            '</span> </div> <div class="win-content">' + this.content +
-            '</div></div>';
-
+            '</span> </div> <div class="win-content" id="win-cnt-' + this.id_win + '"></div></div>';
         $('body').append(gui);
     }
 
+    SetDraggingEvent() {
+        let drag = false;
+        let offsetX = 0;
+        let offsetY = 0;
+        let id = this.id_win;
+
+        $(".win-top").on("mousedown",function(e) {
+            drag = true;
+            offsetX = e.pageX - parseInt($('#win-' + id).css("left"));
+            offsetY = e.pageY - parseInt($('#win-' + id).css("top"));
+        });
+
+        $(".win-top").on("mouseup",function() {
+            drag = false;
+        });
+
+        $("#win-"+ id).on("mousemove", function(e) {
+            if(drag)
+            {
+                $('#win-' + id).css('left', (e.pageX - offsetX) + 'px');
+                $('#win-' + id).css('top', (e.pageY - offsetY) + 'px');
+            }
+        });
+    }
+
     setContent(v) {
-        this.content = v;
+        $("#win-cnt-" + this.id_win).innerHTML = v;
     }
 
     setPosition(x, y) {
