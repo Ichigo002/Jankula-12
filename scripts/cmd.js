0a41f51631cd94a987b4dfcca79729c1ece9cae2
Ichigo002 <wiktorbojanowski28@wp.pl>
2022-04-08 23:17:37 +0200

    add help() prompt command

--- scripts/cmd.js
+++ scripts/cmd.js
@@ -1,10 +1,27 @@
+console.log("---\n For basic commands and variables type command: 'help()'\n---")
+
+function help() {
+    console.log("------ Commands ------ \n"+
+    "* func NewWindow(name, width, height, posX, posY, icon) | Create a new Window on the desktop\n" +
+    "* func ExecWin(name, action) | window with 'name' executes 'action'\n" +
+    "* func getAmountWins()       | return amount of all windows\n" +
+    "------ Vars ------ \n" +
+    "* wins[]         | All existing windows in the website\n" +
+    "* min_width_win  | Minimal width of window\n" +
+    "* min_height_win | Minimal height of window\n" +
+    "* min_z          | Minimal z-index value of active window\n" +
+    "* max_z          | Maximal z-index value of active window\n" +
+    "* z_index        | Last used z-index value by window\n");
+
+    return "executed";
+}
+
 //Create new active window on the desktop
-function NewWindow(name, width, height, posX, posY, icon) 
-{
+function NewWindow(name, width, height, posX, posY, icon) {
     if(name == null)
         name = "Default Window";
     if(icon == null)
-        icon = "|Nan|";
+        icon = "<i class='icon-doc-inv'></i>";
     if(width < this.min_width_win || width == null)
         width = this.min_width_win;
     if(height < this.min_height_win || height == null)
